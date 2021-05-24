e4adc5d69c1d06b009b1538e670bc3a4d019edb6
Umang Mehta <umangrmehta@gmail.com>
2021-05-23 22:55:20 -0400

    Added IMDB and Sentiment140 Dataset Modules

---
+++ dataset/sentiment140.py
@@ -0,0 +1,14 @@
+import tensorflow_datasets as tfds
+
+
+def get_datasets(validation_percent):
+	train_percent = 100 - validation_percent
+
+	train_validation_split = tfds.Split.TRAIN.subsplit([train_percent / 10, validation_percent / 10])
+
+	(train_data, validation_data), test_data = tfds.load(
+		name="sentiment140",
+		split=(train_validation_split, tfds.Split.TEST),
+		as_supervised=True)
+
+	return train_data, validation_data, test_data
