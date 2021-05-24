7a8a2a61f6fd9881023c4e326436c6ce5ae6d6f8
Umang Mehta <umangrmehta@gmail.com>
2021-05-23 22:56:21 -0400

    Setting trainable and pooled outside the ELMo embedding()

--- embeddings/elmo.py
+++ embeddings/elmo.py
@@ -1,8 +1,12 @@
 import tensorflow as tf
 import tensorflow_hub as hub
 
+# Setting outside to let embedding() to be used in Lambda Layer
+trainable = False
+pooled = False
 
-def embeddings(x, trainable: bool = False, pooled: bool = False):
+
+def embeddings(x):
 	elmo = hub.Module("https://tfhub.dev/google/elmo/3", trainable=trainable)
 	if pooled:
 		return elmo(tf.cast(x, tf.string), signature="default", as_dict=True)["default"]
